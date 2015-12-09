910bd0c996b1d44c3418454f4a51a7141c9566af
Chris Garcia <chrisgarcia001@gmail.com>
2015-12-08 23:49:12 -0500

    Updating

--- dna-sequencing-algorithms/week_1.py
+++ dna-sequencing-algorithms/week_1.py
@@ -1,6 +1,25 @@
 from util import *
 from alg_lib import *
 
+def hist(itms):
+	h = {}
+	for itm in itms:
+		if not(h.has_key(itm)):
+			h[itm] = 0
+		h[itm] += 1
+	return h
+
+seqs, quals = readFastq('ERR037900_1.first1000.fastq')
+print('Num Sequences: '  + str(len(seqs)))
+
+
+
+lefts = map(lambda x: strand_aware_exact_matches('C',x)[0], seqs)
+print(hist(lefts))
+#for (i,s) in zip(range(len(seqs)), seqs):
+#	print((i, , strand_aware_exact_matches('G',s)[0]))
+	
+	
 data = readGenome('lambda_virus.fa')
 print('Data length: ' + str(len(data)))
 #print(data)
@@ -13,5 +32,4 @@ print('Q4 - Leftmost AGTCGA in lambda: ' + str(strand_aware_exact_matches('AGTCG
 print('Q5 - TTCAAGCC in lambda (Max 2 mismatches): ' + str(len(strand_aware_exact_matches('TTCAAGCC',data,2))))
 print('Q6 - AGGAGGTT in lambda (Max 2 mismatches): ' + str(len(strand_aware_exact_matches('AGGAGGTT',data,2))))
 
-reads = readFastq('ERR037900_1.first1000.fastq')
 
